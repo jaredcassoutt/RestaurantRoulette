9c91239c960ad6d16d6dbdcf6b2636768b9e3010
jared Cassoutt <jaredcass99@gmail.com>
2020-10-26 23:01:31 -0500

    Initial Commit

---
+++ Restaurant Roulette/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Restaurant Roulette
+//
+//  Created by Jared Cassoutt on 10/26/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
